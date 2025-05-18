f8f067b20c06e35ac71b7a4341e5e4405ca1d5f0
GORGUI\gorgu <gorguindongondiaye@gmail.com>
2025-05-18 15:02:01 -0400

    bug correction inventory2

--- inventory-service/src/main/java/com/gogo/inventory_service/kafka/OrderListener.java
+++ inventory-service/src/main/java/com/gogo/inventory_service/kafka/OrderListener.java
@@ -33,7 +33,7 @@ public class OrderListener {
             productService.saveProduct(product);
             int qtyUsed= event.getProductItemEventDto().getQty();
             int qr=productService.qtyRestante(product.getQty(),qtyUsed,event.getStatus());
-            if(qr>0){
+            if(qr>=0){
                 productService.updateProductQty(event.getProductEventDto().getProductIdEvent(),qr);
 
                 LOGGER.info("Product Update event with updated quantity status sent to order service => {}", event);
