69ac321644af24e6c574e49cf7d8f4e08e61b257
GORGUI\gorgu <gorguindongondiaye@gmail.com>
2025-05-18 15:26:21 -0400

    bug status correction2

--- order-service/src/main/java/com/gogo/order_service/kafka/ProductConsumer.java
+++ order-service/src/main/java/com/gogo/order_service/kafka/ProductConsumer.java
@@ -84,12 +84,16 @@ public class ProductConsumer {
                 productProducer.sendMessage(orderEventDto);
             }
         }
-       /* if(event.getStatus().equalsIgnoreCase(EventStatus.UNAVAILABLE.name())){
+        if(event.getStatus().equalsIgnoreCase(EventStatus.UNAVAILABLE.name())||event.getStatus().equalsIgnoreCase(EventStatus.LOW.name())){
             Product product=orderService.findProductById(event.getProduct().getProductIdEvent());
             if(product.getQty()==0){
                 product.setQtyStatus(EventStatus.UNAVAILABLE.name());
                 productRepository.updateProductQtyStatus(product.getProductIdEvent(),product.getQtyStatus());
             }
-        }*/
+            if(product.getQty()<10){
+                product.setQtyStatus(EventStatus.LOW.name());
+                productRepository.updateProductQtyStatus(product.getProductIdEvent(),product.getQtyStatus());
+            }
+        }
     }
 }
