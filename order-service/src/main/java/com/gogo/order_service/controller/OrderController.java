164df47fe570caf0879ebf850557665c1453d961
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-05-26 13:15:06 -0400

    removed cross origin order

--- order-service/src/main/java/com/gogo/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/gogo/order_service/controller/OrderController.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4300"})
+//@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4300"})
 @RestController
 @RequestMapping("/api/v1")
 public class OrderController {
