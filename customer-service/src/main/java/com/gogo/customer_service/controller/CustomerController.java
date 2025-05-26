9bd2a8d1919df66f3ac67071585edf8a31a82721
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-05-26 13:05:28 -0400

    removed cross origin customer

--- customer-service/src/main/java/com/gogo/customer_service/controller/CustomerController.java
+++ customer-service/src/main/java/com/gogo/customer_service/controller/CustomerController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4300"})
+//@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:4300"})
 @RestController
 @RequestMapping("/api/v1")
 public class CustomerController {
