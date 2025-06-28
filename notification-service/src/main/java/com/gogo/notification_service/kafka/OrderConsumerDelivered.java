b1be72aa6765503c22863d9c0d4dabb1a507fcb2
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-06-27 23:28:33 -0400

    updated notif global7

--- notification-service/src/main/java/com/gogo/notification_service/kafka/OrderConsumerDelivered.java
+++ notification-service/src/main/java/com/gogo/notification_service/kafka/OrderConsumerDelivered.java
@@ -2,7 +2,6 @@ package com.gogo.notification_service.kafka;
 
 import com.gogo.base_domaine_service.event.EventStatus;
 import com.gogo.base_domaine_service.event.OrderEventDto;
-import com.gogo.base_domaine_service.event.ProductItemEventDto;
 import com.gogo.notification_service.model.Notification;
 import com.gogo.notification_service.repository.NotificationRepository;
 import org.slf4j.Logger;
@@ -11,24 +10,25 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderConsumerCancel {
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumerCancel.class);
+public class OrderConsumerDelivered {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumerDelivered.class);
     private final NotificationRepository notificationRepository;
 
-    public OrderConsumerCancel(NotificationRepository notificationRepository) {
+    public OrderConsumerDelivered(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
     }
 
     @KafkaListener(
-            topics = "${spring.kafka.topic.billing.name}",
-            groupId = "${spring.kafka.consumer.group-id}"
+            topics = "${spring.kafka.topic.delivered.name}",
+            groupId = "${notification.kafka.group.delivered}"
     )
     public void orderConsumer(OrderEventDto event) {
         String username = event.getUserName();
         String orderId = event.getId();
 
-        if (EventStatus.CANCELED.name().equalsIgnoreCase(event.getStatus())) {
-            String msg = "The order: " + orderId + " has been cancelled !";
+        if (EventStatus.DELIVERED.name().equalsIgnoreCase(event.getStatus())) {
+            String msg = "The order: " + orderId + " has been delivered successfully!";
 
             Notification userNotif = new Notification();
             userNotif.setMessage(msg);
@@ -36,14 +36,13 @@ public class OrderConsumerCancel {
             userNotif.setUsername(username);
             userNotif.setArchived(false);
             userNotif.setType("user");
-            userNotif.setProductKey("order_" + orderId.toLowerCase() + "_canceled");
+            userNotif.setProductKey("order_" + orderId.toLowerCase() + "_delivered");
 
             notificationRepository.save(userNotif);
 
-            LOGGER.info("📩 Cancelled order notification saved for user: {}", username);
+            LOGGER.info("📬 Delivered order notification saved for user: {}", username);
         }
 
-        LOGGER.info("📩 Order event received in Notification service => {}", event);
+        LOGGER.info("📬 Order event received in Notification service => {}", event);
     }
 }
-
