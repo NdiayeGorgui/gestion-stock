313dbf66731f6f15be5036809c41710698f1a79d
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-09-01 21:44:13 -0400

    mcp client changed

--- mcp-java-client/src/main/java/com/gogo/mcp_java_client/controller/AiWebAPIController.java
+++ mcp-java-client/src/main/java/com/gogo/mcp_java_client/controller/AiWebAPIController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-@CrossOrigin("*")
+@RequestMapping("/api/v1")
 public class AiWebAPIController {
     private MyAIAgent agent;
 
