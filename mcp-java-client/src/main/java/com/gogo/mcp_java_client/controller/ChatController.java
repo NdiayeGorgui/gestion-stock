e3be379f12c811b4d5b4c3b96272112ed32eb850
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-09-04 12:39:35 -0400

    Update ChatController.java

--- mcp-java-client/src/main/java/com/gogo/mcp_java_client/controller/ChatController.java
+++ mcp-java-client/src/main/java/com/gogo/mcp_java_client/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.gogo.mcp_java_client.controller;
 import com.gogo.mcp_java_client.agents.MyAIAgent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @RequestMapping("/api/v1")
 @RestController
